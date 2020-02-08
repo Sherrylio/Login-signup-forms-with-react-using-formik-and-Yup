4ab027110ff137b605cad2b587a85260005a2a2e
Sherrylio <muhammadsheharyar1012@gmail.com>
2020-02-08 19:59:17 +0500

    Updates

---
+++ precache-manifest.4b589a6e61610e3a4d19d1d255bb6a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59dd2726273f403ebe70acd71437dac2",
+    "url": "/Login-signup-forms-with-react-using-formik-and-Yup/index.html"
+  },
+  {
+    "revision": "2643764a69157308fcd5",
+    "url": "/Login-signup-forms-with-react-using-formik-and-Yup/static/css/main.a3990db3.chunk.css"
+  },
+  {
+    "revision": "fcb2cda526bd71a7a8c2",
+    "url": "/Login-signup-forms-with-react-using-formik-and-Yup/static/js/2.115d3ef6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Login-signup-forms-with-react-using-formik-and-Yup/static/js/2.115d3ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2643764a69157308fcd5",
+    "url": "/Login-signup-forms-with-react-using-formik-and-Yup/static/js/main.be59c632.chunk.js"
+  },
+  {
+    "revision": "ce2074750f9e69967327",
+    "url": "/Login-signup-forms-with-react-using-formik-and-Yup/static/js/runtime-main.26414492.js"
+  }
+]);
